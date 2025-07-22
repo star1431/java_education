37bf97f7fcee9fa90219f4315d2368837d8c9269
star1431 <star1431@naver.com>
2025-07-22 13:06:23 +0900

    test

---
+++ javaEDU_0722/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numVal = 5;
+        for(int i=0; i < numVal; i++) {
+            System.out.println(numVal);
+        }
+        System.out.println("Hello, World!");
+    }
+}
---
+++ javaEDU_0722/src/HelloWorld.java
@@ -0,0 +1,14 @@
+// class(생성자) : 변수(필드), 메서드(함수) 등을 정의할 수 있음
+public class HelloWorld {
+    // 클래스내 지역변수
+    // 메인 메서드
+    public static void main(String[] args) {
+        // 변수(필드) 선언
+        String strVal = "Hello";
+        int numVal = 5;
+        // 메서드 호출 (System.out.println)
+        System.out.println("소녀시대");
+        System.out.println(strVal);
+        System.out.println(numVal);
+    }
+}
