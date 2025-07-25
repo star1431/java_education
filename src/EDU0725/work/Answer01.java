4fe19bc09bc1efd4c46e4184425a0842ece042fd
star1431 <star1431@naver.com>
2025-07-25 14:52:21 +0900

    commit

--- src/EDU0725/work/Answer01.java
+++ src/EDU0725/work/Answer01.java
@@ -26,14 +26,12 @@ public class Answer01 {
     /**
      * utilNumType : 홀수,짝수 판단 함수
      * @param num 대상 값
-     * @return "짝수" or "홀수"
+     * @return true : 짝수 | false : 홀수
      */
-    public static String utilNumType(int num) {
-        String returnData = "결과없음";
+    public static boolean utilNumType(int num) {
+        boolean returnData = false;
         if(num % 2 == 0) {
-            returnData = "짝수";
-        } else {
-            returnData = "홀수";
+            returnData = true;
         }
         return returnData;
     }
@@ -93,12 +91,14 @@ public class Answer01 {
      * @return min 이상 max 이하의 랜덤 정수로 구성된 배열
      */
     public static int[] utilRandomArray(int min, int max, int leng) {
-        int[] returnData = new int[leng];   // 배열 생성
+        int[] returnData = new int[leng];
         for (int i = 0; i < leng; i++) {
+            returnData[i] = (int) (Math.random() * (max - min + 1)) + min;
             /** 
-             * Math.random()                : 0 ~ 1 사이의 랜덤 값 (실수값 : 0 ~ 0.9999...)
-             * (int) Math.random()          : 정수 변환 
-             *   -> (int) Math.random() * 10 이면 0 ~ 9 사이의 정수
+             * Math.random()        : 0 ~ 1 사이의 랜덤 값 (실수값 : 0 ~ 0.9999...)
+             * (int) Math.random()  : 정수 변환 
+             * 예시
+             * => (int) (Math.random()*3) => (int) (0.0 ~ 2.9999...) => 0, 1, 2 중 하나
              * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
              * 계산 원리
              * 1. 최소(min) = -9 / 최대(max) = 2 가정 ( -9 ~ 2 범위 )
@@ -109,7 +109,6 @@ public class Answer01 {
              *  = 2 - 9
              *  = -7
              */
-            returnData[i] = (int) (Math.random() * (max - min + 1)) + min;
         }
         return returnData;
     }
@@ -120,8 +119,8 @@ public class Answer01 {
         System.out.println("문제1 : 정수 짝수 ? 홀수 ? 판단 함수");
         int num1 = 5;
         int num2 = 10;
-        System.out.println("num1 = " + num1 + " " + utilNumType(num1));
-        System.out.println("num2 = " + num2 + " " + utilNumType(num2));
+        System.out.println("num1 = " + num1 + " 짝수 ? -> " + utilNumType(num1));
+        System.out.println("num2 = " + num2 + " 짝수 ? -> " + utilNumType(num2));
     }
 
     // example02 : 문제2
@@ -137,14 +136,17 @@ public class Answer01 {
     public void example03() {
         System.out.println("문제3 : 같은 크기 배열 2개 입력 받아 두 배열의 각 인덱스끼리 합한 새로운 배열로 리턴하는 함수");
         // ex: {1,2,3,4,5}, {4,5,6,7,8} -> {5,7,9,11,13}
+
         int[] arr1 = new int[] {1, 2, 3, 4, 5};
         int[] arr2 = new int[] {4, 5, 6, 7, 8, 9};
-        int[] result = utilSumArray(arr1, arr2);
+        int[] result = utilSumArray(arr1, arr2); //  가비지컬렉션 방지 - 할당
+
         System.out.println("arr1 와 arr2 요소 합 배열 : " + Arrays.toString(result));
         /**
-         * toString() ? = 받은 객체를 문자열로 변환
-         * Arrays.toString() : 배열 전용 // [, , , ] 형태로 변환
-         * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+         * obj.toString()       : 받은 객체를 문자열로 변환
+         * Arrays.toString()    : 배열 전용 // [, , , ] 형태로 변환
+         * ㅡㅡㅡ
+         * 상단에 import java.util.Arrays; 추가 필요 
          */
     }
     // example04 : 문제4
@@ -152,8 +154,13 @@ public class Answer01 {
         System.out.println("문제4 : 랜덤한 값 6개가 저장된 배열을 리턴하는 함수");
         int min = 1;
         int max = 50;
-        // System.out.println("랜덤 배열 1 : " + Arrays.toString(utilRandomArray(min, max, 6)));
-        // System.out.println("랜덤 배열 2 : " + Arrays.toString(utilRandomArray(min, max, 6)));
-        // System.out.println("랜덤 배열 3 : " + Arrays.toString(utilRandomArray(min, max, 6)));
+        // 가비지컬렉션 방지 - 할당
+        int[] ranArr01 = utilRandomArray(min, max, 6);
+        int[] ranArr02 = utilRandomArray(min, max, 6);
+        int[] ranArr03 = utilRandomArray(min, max, 6);
+        
+        System.out.println("랜덤 배열 1 : " + Arrays.toString(ranArr01));
+        System.out.println("랜덤 배열 2 : " + Arrays.toString(ranArr02));
+        System.out.println("랜덤 배열 3 : " + Arrays.toString(ranArr03));
     }
 }
